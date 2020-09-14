f337a0386d20ad7d4ef8633e62ba1822f4fe734b
pedro.santos <pedroabs.dev42@gmail.com>
2020-09-14 00:24:50 -0300

    Custom Exceptions

---
+++ src/main/java/com/pedrosantos/workshopmongo/resource/exception/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.pedrosantos.workshopmongo.resource.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.pedrosantos.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado",
+                e.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(err.getStatus()).body(err);
+    }
+}
--- src/main/java/com/pedrosantos/workshopmongo/services/UserService.java
+++ src/main/java/com/pedrosantos/workshopmongo/services/UserService.java
@@ -1,9 +1,11 @@
 package com.pedrosantos.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.pedrosantos.workshopmongo.domain.User;
 import com.pedrosantos.workshopmongo.repository.UserRepository;
+import com.pedrosantos.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,12 @@ public class UserService {
     
     @Autowired
     private UserRepository repo;
+
+    public User findById(String id)  {
+        Optional<User> obj = repo.findById(id);
+
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
 
     public List<User> findAll() {
         return repo.findAll();
