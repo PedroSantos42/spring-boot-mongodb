0f40a794eec9cec397a35afb166535b65127e391
pedro.santos <pedroabs.dev42@gmail.com>
2020-09-23 17:06:23 -0300

    AuthorDTO

--- src/main/java/com/pedrosantos/workshopmongo/config/Instantiation.java
+++ src/main/java/com/pedrosantos/workshopmongo/config/Instantiation.java
@@ -6,6 +6,7 @@ import java.util.TimeZone;
 
 import com.pedrosantos.workshopmongo.domain.Post;
 import com.pedrosantos.workshopmongo.domain.User;
+import com.pedrosantos.workshopmongo.dto.AuthorDTO;
 import com.pedrosantos.workshopmongo.repository.PostRepository;
 import com.pedrosantos.workshopmongo.repository.UserRepository;
 
@@ -34,11 +35,11 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-        Post post1 = new Post(null, sdf.parse("27/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo, abraços...", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", maria);
-
+        
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        
+        Post post1 = new Post(null, sdf.parse("27/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo, abraços...", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
         
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
