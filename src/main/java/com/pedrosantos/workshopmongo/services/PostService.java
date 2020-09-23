988eef834518ee69416ddebf047d0721c59c5938
pedro.santos <pedroabs.dev42@gmail.com>
2020-09-23 19:08:04 -0300

    Listing Post with query methods

--- src/main/java/com/pedrosantos/workshopmongo/services/PostService.java
+++ src/main/java/com/pedrosantos/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.pedrosantos.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.pedrosantos.workshopmongo.domain.Post;
@@ -19,5 +20,9 @@ public class PostService {
         Optional<Post> obj = repo.findById(id);
 
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 }
