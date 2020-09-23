7291df62d4ccc45274c72a879f4e046a970ce821
pedro.santos <pedroabs.dev42@gmail.com>
2020-09-23 17:22:32 -0300

    Endpoint GET Post by ID

---
+++ src/main/java/com/pedrosantos/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.pedrosantos.workshopmongo.services;
+
+import java.util.Optional;
+
+import com.pedrosantos.workshopmongo.domain.Post;
+import com.pedrosantos.workshopmongo.repository.PostRepository;
+import com.pedrosantos.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+    
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id)  {
+        Optional<Post> obj = repo.findById(id);
+
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
