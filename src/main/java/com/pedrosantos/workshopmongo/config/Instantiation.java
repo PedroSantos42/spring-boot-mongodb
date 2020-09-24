c39dfaae0973ed455df93d3f89388ad81924b621
pedro.santos <pedroabs.dev42@gmail.com>
2020-09-23 23:53:42 -0300

    Posts fullsearch

--- src/main/java/com/pedrosantos/workshopmongo/config/Instantiation.java
+++ src/main/java/com/pedrosantos/workshopmongo/config/Instantiation.java
@@ -39,7 +39,7 @@ public class Instantiation implements CommandLineRunner {
         
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
         
-        Post post1 = new Post(null, sdf.parse("27/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo, abraços...", new AuthorDTO(maria));
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo, abraços...", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
         
         CommentDTO comment1 = new CommentDTO("Boa viagem, mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
--- src/main/java/com/pedrosantos/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/pedrosantos/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.pedrosantos.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import com.pedrosantos.workshopmongo.domain.Post;
@@ -10,9 +11,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-    
+
     @Query("{ 'title': { $regex: ?0, $options: 'i'} }")
     List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} }, { $or:  [   { 'title': { $regex: ?0, $options: 'i'} }, { 'body': { $regex: ?0, $options: 'i'} }, { 'comments.text': { $regex: ?0, $options: 'i'} } ] }]}")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
